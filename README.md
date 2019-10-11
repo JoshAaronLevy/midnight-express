0a553ea2038e81fe049ff6d0a370bf15a3236bde
Josh Levy <joshaaronlevy@gmail.com>
2019-10-11 05:11:55 -0500

    got base options with conditions set up. working on functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Commander Test
+# Midnight Express CLI
+
+## Getting Started
+
+To install:
+
+`npm i -g midnight-express`
+
+To set up a new midnight-express project:
+
+`midnight-express new`
--- index.js
+++ index.js
@@ -2,28 +2,107 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
-'use strict';
-var inquirer = require('inquirer');
+const program = require('commander');
+('use strict');
+const inquirer = require('inquirer');
 
-var questions = [
+let questions = [
   {
     type: 'input',
     name: 'project_name',
-    message: "Project name"
-  }
+    message: 'Project name'
+  },
+  {
+    type: 'confirm',
+    name: 'routeFolder',
+    message: 'Would you like to set up an initial custom route? (Recommended)',
+  },
+  {
+    type: 'input',
+    name: 'routeName',
+    message: 'What is the name of your custom route? (i.e. dogs)',
+    when: function(answers) {
+      return answers.routeFolder;
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'dbSetup',
+    message: 'Would you like to set up a database?',
+  },
+  {
+    type: 'list',
+    name: 'dbType',
+    message: 'What type of database would you like to use?',
+    choices: [
+      'SQL',
+      'NoSQL'
+    ],
+    when: function(answers) {
+      return answers.dbSetup;
+    }
+  },
+  {
+    type: 'list',
+    name: 'SQLSelect',
+    message: 'What SQL database would you like to install?',
+    choices: [
+      'PostgreSQL',
+      'SQLite',
+      'MySQL',
+      'MariaDB',
+      'Other'
+    ],
+    when: function(answers) {
+      return answers.dbType === 'SQL';
+    }
+  },
+  {
+    type: 'list',
+    name: 'NoSQLSelect',
+    message: 'What NoSQL database would you like to install?',
+    choices: [
+      'MongoDB',
+      'Other'
+    ],
+    when: function(answers) {
+      return answers.dbType === 'NoSQL';
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'knexSetup',
+    message: 'Would you like to use knex?',
+    when: function(answers) {
+      return answers.dbType === 'SQL';
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'mongooseSetup',
+    message: 'Would you like to use mongoose?',
+    when: function(answers) {
+      return answers.dbType === 'NoSQL';
+    }
+  },
+  {
+    type: 'confirm',
+    name: 'testingSetup',
+    message: 'Would you like to add testing (Mocha/Chai)?',
+  },
 ];
 
 inquirer.prompt(questions).then(answers => {
-  const projName = answers.project_name
+  const projName = answers.project_name;
   console.log(JSON.stringify(answers, null, 2));
   checkRequestedInput(projName);
 });
 
 function checkRequestedInput(projName) {
-  const basePath = ensureDirectory(projName)
-  createTemplateFile(basePath, 'index.js')
-  createTemplateFile(basePath, 'README.md', projName)
-  createTemplateFile(basePath, '.gitignore')
+  const basePath = ensureDirectory(projName);
+  createTemplateFile(basePath, 'index.js');
+  createTemplateFile(basePath, 'README.md', projName);
+  createTemplateFile(basePath, '.gitignore');
 }
 
 function ensureDirectory(pathName) {
@@ -43,8 +122,7 @@ function createTemplateFile(basePath, fileName, projName) {
       fs.writeFileSync(targetFile, '', 'utf8');
     }
   } else {
-    console.error('OOPs file already exists:', targetFile);
+    console.error(`File ${targetFile} already exists`);
   }
   return targetFile;
 }
-

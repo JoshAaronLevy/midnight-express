42c98b8b6223f6748a03b28350a4654975429fdf
Josh Levy <joshaaronlevy@gmail.com>
2019-10-12 01:43:41 -0600

    Got base route code with static routes generating

--- templates/express.js
+++ templates/express.js
@@ -1,58 +1,53 @@
 const handlebars = require('handlebars');
 
-module.exports = async (route) => {
-  const indexFile = await(route);
-  handlebars.compile(
-    `
-    const express = require('express');
-    const handlebars = require('handlebars');
-    const router = express.Router();
+const templateSpec = `
+const express = require('express');
+const router = express.Router();
 
-    const queries = require('../queries');
+const queries = require('../queries');
 
-    router.get('/', (request, response, next) => {
-      queries.list('${route}').then(${route} => {
-        response.json({
-          ${route}
-        });
-      }).catch(next);
+router.get('/', (request, response, next) => {
+  queries.list('route').then(route => {
+    response.json({
+      route
     });
+  }).catch(next);
+});
 
-    router.get('/:id', (request, response, next) => {
-      queries.read(request.params.id).then(${route} => {
-        ${route}
-          ?
-          response.json({
-            ${route}
-          }) :
-          response.sendStatus(404)
-      }).catch(next);
+router.get('/:id', (request, response, next) => {
+  queries.read(request.params.id).then(route => {
+    route
+      ?
+      response.json({
+        route
+      }) :
+      response.sendStatus(404)
+  }).catch(next);
+});
+
+router.post('/', (request, response, next) => {
+  queries.create(request.body).then(route => {
+    response.status(201).json({
+      route: route
     });
+  }).catch(next);
+});
 
-    router.post('/', (request, response, next) => {
-      queries.create(request.body).then(${route} => {
-        response.status(201).json({
-          ${route}: ${route}
-        });
-      }).catch(next);
+router.put('/:id', (request, response, next) => {
+  queries.update(request.params.id, request.body).then(route => {
+    response.json({
+      route: route[0]
     });
+  }).catch(next);
+});
 
-    router.put('/:id', (request, response, next) => {
-      queries.update(request.params.id, request.body).then(${route} => {
-        response.json({
-          ${route}: ${route}[0]
-        });
-      }).catch(next);
-    });
+router.delete('/:id', (request, response, next) => {
+  queries.delete(request.params.id).then(() => {
+    response.sendStatus(204);
+  }).catch(next);
+});
 
-    router.delete('/:id', (request, response, next) => {
-      queries.delete(request.params.id).then(() => {
-        response.sendStatus(204);
-      }).catch(next);
-    });
+module.exports = router;
+`
 
-    module.exports = router;
-    `
-  )
-  console.log(indexFile)
-}
+module.exports = templateSpec
